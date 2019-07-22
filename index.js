e6c3e18bd67825314f70c488a7d447a582632d0d
JonathanTech <jonathan.ross.contractor@macmillan.com>
2019-07-21 19:53:39 -0500

    new stuff

---
+++ index.js
@@ -0,0 +1,19 @@
+let rpgio = require('rpi-gpio')
+let rp = rpgio.promise
+
+let toggle = false
+
+const magReader = 11
+const relay = 13
+
+rp.setup(relay, rpgio.DIR_LOW)
+  .then(() => rp.setup(magReader, rpgio.DIR_IN, rpgio.EDGE_BOTH))
+  .then(() => {
+    setInterval(() =>{
+      rp.read(magReader).then(value =>{
+
+        rp.write(relay, !!value)
+      })
+    }, 10)
+  })
+  .catch(err => console.error(err))
